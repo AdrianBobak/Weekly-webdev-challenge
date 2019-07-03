181010a89bbdb94d21b579f04f8264b4ea80b662
Adrian Bobak <bobak.adrian@gmail.com>
2019-07-03 13:41:26 +0200

    add toggler function to toggle menus and bars

---
+++ 04/js/scripts.js
@@ -0,0 +1,22 @@
+const togglerInit = () => {
+    const toggler = (e) => {
+
+        let btn = e.target;
+    
+        if(!btn.dataset.target){
+            btn = e.target.parentNode;
+        }
+    
+        const targets = document.querySelectorAll(btn.dataset.target);
+        targets.forEach((target) => {
+            target.classList.toggle('active');
+        });
+    }
+    
+    const elems = document.querySelectorAll('[data-target]');
+    elems.forEach((elem) => {
+        elem.addEventListener('click', toggler);
+    });
+}
+
+window.addEventListener('load', togglerInit);
