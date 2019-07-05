6a4903497919050ef1b0974a7fd6e11749495c95
Adrian Bobak <bobak.adrian@gmail.com>
2019-07-05 11:21:51 +0200

    add submenuLoader function

--- 04/js/scripts.js
+++ 04/js/scripts.js
@@ -1,30 +1,9 @@
+import {togglerInit} from './navToggler.js';
+import {submenuLoader} from './submenuLoader.js';
 import Glide from './glide.esm.js';
 
-const togglerInit = () => {
-    const toggler = (e) => {
-
-        let btn = e.target;
-    
-        if(!btn.dataset.target){
-            btn = e.target.parentNode;
-        }
-    
-        const targets = document.querySelectorAll(btn.dataset.target);
-        targets.forEach((target) => {
-            target.classList.toggle('active');
-        });
-    }
-    
-    const elems = document.querySelectorAll('[data-target]');
-    elems.forEach((elem) => {
-        elem.addEventListener('click', toggler);
-    });
-}
-
 window.addEventListener('load', togglerInit);
-
-
-// SLIDER
+window.addEventListener('load', submenuLoader);
 
 new Glide('.glide', {
     autoplay: 5000,
