28b23972549630ceb3aaffd352bd3f8269097b36
Adrian Bobak <bobak.adrian@gmail.com>
2019-07-09 18:55:19 +0200

    add testimonials scripts

--- 05/js/scripts.js
+++ 05/js/scripts.js
@@ -1,7 +1,9 @@
 import {toggler} from './toggler.js';
 import {backToTop} from './backToTop.js';
 import {portfolio} from './portfolio.js';
+import {testimonials} from './testimonials.js';
 
 window.addEventListener('load', toggler);
 window.addEventListener('load', backToTop);
 window.addEventListener('load', portfolio);
+window.addEventListener('load', testimonials);
---
+++ 05/js/testimonials.js
@@ -0,0 +1,52 @@
+export const testimonials = () => {
+    const elems = document.querySelectorAll('[data-testimonialTarget]');
+    const bullets = document.querySelectorAll('[data-testimonial]');
+
+    const switchTestimonial = e => {
+        let bullet = e.target;
+        if(!bullet.dataset.testimonial) bullet = e.target.parentNode;
+        const target = bullet.dataset.testimonial;
+
+        const transition = parseFloat(getComputedStyle(elems[0]).getPropertyValue('transition-duration')) * 1000;
+        let toRemove;
+        let toActivate;
+
+        elems.forEach((elem, index) => {
+            if(elem.classList.contains('active')){
+                if(elem.dataset.testimonialtarget === target) return;
+                else toRemove = index;
+            }
+            if(elem.dataset.testimonialtarget === target) toActivate = index;
+        });
+        
+        if(Number.isInteger(toRemove) && Number.isInteger(toActivate)){
+            execute(elems[toActivate], elems[toRemove], transition);
+            bulletsActive(bullets, bullet);
+        }
+    }
+
+    bullets.forEach(bullet => {
+        bullet.addEventListener('click', switchTestimonial);
+    });
+}
+
+const execute = (toActivate, toRemove, transition = 2000) => {
+    toRemove.style.opacity = "0";
+    toActivate.style.opacity = "0";
+
+    setTimeout(function(){
+        toRemove.classList.remove('active');
+        toActivate.classList.add('active');
+
+        setTimeout(function(){
+            toActivate.style.opacity = "1";
+        }, (transition / 2));
+    }, (transition / 2));
+}
+
+const bulletsActive = (bullets, activeBullet) => {
+    bullets.forEach(elem => {
+        if(elem === activeBullet) elem.classList.add('active');
+        else elem.classList.remove('active');
+    });
+}
