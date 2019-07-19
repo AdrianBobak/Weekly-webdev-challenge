e506fef8e22a8b2f16fa89e1694f8a3d93c38805
Adrian Bobak <bobak.adrian@gmail.com>
2019-07-19 13:54:00 +0200

    add and configure glide slider

--- 06/js/scripts.js
+++ 06/js/scripts.js
@@ -1,5 +1,11 @@
 import {dataActivate} from './dataActivate.js';
+import Glide from './glide.esm.js';
 
 window.addEventListener('load', function(){
     dataActivate('activate'); // argument: name of dataset
 });
+
+new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+}).mount();
